d9d170f259bffe9e6cd3e6d5a6bac1c417c92345
judithgispert <jugispert@gmail.com>
2024-02-20 19:40:49 +0100

    Generate random num

---
+++ src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t02/n01/mongodb/methods/DiceRandomNum.java
@@ -0,0 +1,7 @@
+package cat.itacademy.barcelonactiva.gispert.judith.s05.t02.n01.mongodb.methods;
+
+public class DiceRandomNum {
+    public static int randomNum(){
+        return (int)(Math.random()*6+1);
+    }
+}
