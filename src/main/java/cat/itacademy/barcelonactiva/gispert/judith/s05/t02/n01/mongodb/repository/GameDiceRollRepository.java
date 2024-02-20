d724229a95ff07c01a6b4d1fe08bcdf0a87524b4
judithgispert <jugispert@gmail.com>
2024-02-20 19:49:47 +0100

    add @Repo

--- src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t02/n01/mongodb/repository/GameDiceRollRepository.java
+++ src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t02/n01/mongodb/repository/GameDiceRollRepository.java
@@ -2,6 +2,8 @@ package cat.itacademy.barcelonactiva.gispert.judith.s05.t02.n01.mongodb.reposito
 
 import cat.itacademy.barcelonactiva.gispert.judith.s05.t02.n01.mongodb.model.domain.GameDiceRoll;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GameDiceRollRepository extends MongoRepository<GameDiceRoll, String> {
 }
