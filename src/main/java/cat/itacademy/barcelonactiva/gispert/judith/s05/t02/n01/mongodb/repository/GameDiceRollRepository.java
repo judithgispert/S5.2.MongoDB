965b30e14a44a7f0884615397f850c8cb2be2b1c
judithgispert <jugispert@gmail.com>
2024-02-24 17:01:02 +0100

    Add new method

--- src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t02/n01/mongodb/repository/GameDiceRollRepository.java
+++ src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t02/n01/mongodb/repository/GameDiceRollRepository.java
@@ -1,9 +1,13 @@
 package cat.itacademy.barcelonactiva.gispert.judith.s05.t02.n01.mongodb.repository;
 
 import cat.itacademy.barcelonactiva.gispert.judith.s05.t02.n01.mongodb.model.domain.GameDiceRoll;
+import cat.itacademy.barcelonactiva.gispert.judith.s05.t02.n01.mongodb.model.domain.Player;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GameDiceRollRepository extends MongoRepository<GameDiceRoll, String> {
+    List<GameDiceRoll> findByPlayer(Player player);
 }
