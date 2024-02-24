cb7a6f30c95482d280a3fcf2f0865d960e4c5a63
judithgispert <jugispert@gmail.com>
2024-02-24 17:00:45 +0100

    Change names

--- src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t02/n01/mongodb/model/dto/PlayerDTO.java
+++ src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t02/n01/mongodb/model/dto/PlayerDTO.java
@@ -1,22 +1,27 @@
 package cat.itacademy.barcelonactiva.gispert.judith.s05.t02.n01.mongodb.model.dto;
 
+import cat.itacademy.barcelonactiva.gispert.judith.s05.t02.n01.mongodb.model.domain.GameDiceRoll;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class PlayerDTO {
-    private String id;
-    private String name;
-    private LocalDateTime registrationDate = LocalDateTime.now();
+    private String idDTO;
+    private String nameDTO;
+    private LocalDateTime registrationDateDTO = LocalDateTime.now();
+    private List<GameDiceRoll> gamesDTO = new ArrayList<>();
 
-    private int gamesWon;
-    private double percentageWon;
-    private int gamesLost;
-    private double percentageLost;
+    private int gamesWonDTO;
+    private double percentageWonDTO;
+    private int gamesLostDTO;
+    private double percentageLostDTO;
 }
