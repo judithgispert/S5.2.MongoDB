c15e88e307eaff462f56ab8fa1fc6f68eeb047b2
judithgispert <jugispert@gmail.com>
2024-02-24 17:01:36 +0100

    Create interfaces

---
+++ src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t02/n01/mongodb/service/GameDiceRollService.java
@@ -0,0 +1,14 @@
+package cat.itacademy.barcelonactiva.gispert.judith.s05.t02.n01.mongodb.service;
+
+import cat.itacademy.barcelonactiva.gispert.judith.s05.t02.n01.mongodb.model.domain.Player;
+import cat.itacademy.barcelonactiva.gispert.judith.s05.t02.n01.mongodb.model.dto.GameDiceRollDTO;
+
+import java.util.List;
+
+public interface GameDiceRollService {
+    GameDiceRollDTO createGame(Player player);
+    GameDiceRollDTO addGame(Player player);
+    List<GameDiceRollDTO> getPlayerGames(Player player);
+    void deleteGames(Player player);
+
+}
---
+++ src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t02/n01/mongodb/service/PlayerService.java
@@ -0,0 +1,25 @@
+package cat.itacademy.barcelonactiva.gispert.judith.s05.t02.n01.mongodb.service;
+
+import cat.itacademy.barcelonactiva.gispert.judith.s05.t02.n01.mongodb.model.domain.Player;
+import cat.itacademy.barcelonactiva.gispert.judith.s05.t02.n01.mongodb.model.dto.GameDiceRollDTO;
+import cat.itacademy.barcelonactiva.gispert.judith.s05.t02.n01.mongodb.model.dto.PlayerDTO;
+
+import java.util.List;
+
+public interface PlayerService {
+    PlayerDTO createPlayer();
+    void addPlayer();
+    List<Player> getPlayers();
+    PlayerDTO updatePlayer(PlayerDTO newPlayerDTO, String id);
+
+    GameDiceRollDTO play (String id);
+    void updateResultGame(GameDiceRollDTO gameDiceRollDTO, PlayerDTO playerDTO);
+    List<GameDiceRollDTO> getGames(String id);
+    void deleteGames(String id);
+    PlayerDTO restartPercentage(PlayerDTO playerDTO);
+    List<PlayerDTO> getRanking();
+    double getPercentageRanking();
+    PlayerDTO getLoser();
+    PlayerDTO getWinner();
+
+}
