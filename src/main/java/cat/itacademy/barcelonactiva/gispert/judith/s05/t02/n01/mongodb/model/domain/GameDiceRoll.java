17f47a0f2237fd266c39ed89837a0568b163207b
judithgispert <jugispert@gmail.com>
2024-02-24 17:00:08 +0100

    Fixing errors

--- src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t02/n01/mongodb/model/domain/GameDiceRoll.java
+++ src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t02/n01/mongodb/model/domain/GameDiceRoll.java
@@ -1,5 +1,6 @@
 package cat.itacademy.barcelonactiva.gispert.judith.s05.t02.n01.mongodb.model.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,7 +20,7 @@ public class GameDiceRoll {
     private int dice2;
     @JsonProperty("isWin")
     private boolean isWin;
-    @JsonProperty("player")
+    @JsonIgnoreProperties({"games"})
     private Player player;
 
     public GameDiceRoll(Player player){
