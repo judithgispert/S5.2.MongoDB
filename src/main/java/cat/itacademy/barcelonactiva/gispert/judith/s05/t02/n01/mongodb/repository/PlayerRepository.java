739cf875baffb29e29b2b337699a9a8fadabb02f
judithgispert <jugispert@gmail.com>
2024-02-24 17:01:21 +0100

    Add new method

--- src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t02/n01/mongodb/repository/PlayerRepository.java
+++ src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t02/n01/mongodb/repository/PlayerRepository.java
@@ -4,6 +4,9 @@ import cat.itacademy.barcelonactiva.gispert.judith.s05.t02.n01.mongodb.model.dom
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PlayerRepository extends MongoRepository<Player, String> {
+    Optional<Player> findByName(String name);
 }
